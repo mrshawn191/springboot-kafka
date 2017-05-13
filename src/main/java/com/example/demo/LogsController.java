685e0c23a8ce652eb70dcfc8fa6e4eae316f8eac
Jun Huh <juan.huh@hotmail.com>
2017-05-13 23:44:02 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/LogsController.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.http.HttpStatus;
+import org.springframework.integration.syslog.inbound.RFC6587SyslogDeserializer;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.GenericMessage;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+@RestController
+public class LogsController implements BeanFactoryAware
+{
+
+    private BeanFactory context;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException
+    {
+        this.context = beanFactory;
+    }
+
+    @RequestMapping(value = "/api/logs/health", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public void healthCheck()
+    {
+    }
+
+    @RequestMapping(value = "/api/logs/create", method = RequestMethod.POST)
+    public String logs(@RequestBody String body) throws IOException
+    {
+        // "application/logplex-1" does not conform to RFC5424.
+        // It leaves out STRUCTURED-DATA but does not replace it with
+        // a NILVALUE. To workaround this, we inject empty STRUCTURED-DATA.
+        String[] parts = body.split("router - ");
+        String log = parts[0] + "router - [] " + (parts.length > 1 ? parts[1] : "");
+
+        // Knows how to speak the language of syslog => turn syslog => into a map => into a json => into a string
+        RFC6587SyslogDeserializer parser = new RFC6587SyslogDeserializer();
+        InputStream is = new ByteArrayInputStream(log.getBytes());
+        Map<String, ?> messages = parser.deserialize(is);
+
+        // Parse json to string
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(messages);
+
+        // Get Producer through dependency injection
+        MessageChannel toKafka = context.getBean("toKafka", MessageChannel.class);
+
+        // Sends message through kafka producer
+        // Send is not async, kafka producer in a background thread will queue up each message and send into the broker in batches
+        // Send them in batches of the type of message that is sent
+        toKafka.send(new GenericMessage<>(json));
+
+        return "ok";
+    }
+
+}
---
+++ src/main/java/com/example/demo/config/KafkaConfig.java
@@ -0,0 +1,146 @@
+package com.example.demo.config;
+
+import com.github.jkutner.EnvKeyStore;
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KafkaConfig
+{
+
+    public static String getTopic()
+    {
+        return "logs";
+    }
+
+    public static String getMessageKey()
+    {
+        return "logs.key";
+    }
+
+    public static Map<String, Object> getProducerProperties()
+    {
+        Map<String, Object> properties = getDefaultKafkaProperties();
+        properties.put(ProducerConfig.ACKS_CONFIG, "all");
+        properties.put(ProducerConfig.RETRIES_CONFIG, 0);
+        properties.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        properties.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        properties.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        // Sets how to serialize key
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // Sets how to serialize value
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    public static Map<String, Object> getConsumerProperties()
+    {
+        Map<String, Object> properties = getDefaultKafkaProperties();
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "dev");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        // Sets how to deserialize key
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        // Sets how to deserialize value
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        return properties;
+    }
+
+    /**
+     * Gets default kafka properties for both ssl and non-ssl
+     */
+    private static Map<String, Object> getDefaultKafkaProperties()
+    {
+        Map<String, Object> properties = new HashMap<>();
+        List<String> hostPorts = new ArrayList<>();
+
+        String kafkaUrl = getEnv("KAFKA_URL");
+        for (String url : kafkaUrl.split(","))
+        {
+            try
+            {
+                URI uri = new URI(url);
+                hostPorts.add(String.format("%s:%d", uri.getHost(), uri.getPort()));
+
+                switch (uri.getScheme())
+                {
+                    case "kafka":
+                        setKafkaProperties(properties);
+                        break;
+                    case "kafka+ssl":
+                        try
+                        {
+                            setKafkaSSLProperties(properties);
+                        }
+                        catch (Exception e)
+                        {
+                            throw new RuntimeException("Failed to create kafka key stores", e);
+                        }
+                        break;
+                    default:
+                        throw new IllegalArgumentException(String.format("unknown scheme: %s", uri.getScheme()));
+                }
+            }
+            catch (URISyntaxException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+
+        properties.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, String.join(",", hostPorts));
+        return properties;
+    }
+
+    /**
+     * Sets non-ssl kafka properties
+     */
+    private static void setKafkaProperties(Map<String, Object> properties)
+    {
+        properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "PLAINTEXT");
+    }
+
+    /**
+     * Sets ssl kafka properties
+     */
+    private static void setKafkaSSLProperties(Map<String, Object> properties) throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException
+    {
+        EnvKeyStore envTrustStore = EnvKeyStore.createWithRandomPassword("KAFKA_TRUSTED_CLIENT");
+        EnvKeyStore envKeyStore = EnvKeyStore.createWithRandomPassword("KAFKA_CLIENT_CERT_KEY", "KAFKA_CLIENT_CERT");
+
+        properties.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, envTrustStore.type());
+        properties.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, envTrustStore.storeTemp().getAbsolutePath());
+        properties.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, envKeyStore.password());
+        properties.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, envKeyStore.type());
+        properties.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, envKeyStore.storeTemp().getAbsolutePath());
+        properties.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, envKeyStore.password());
+    }
+
+    /**
+     * Returns the environmental variable for the given url
+     */
+    private static String getEnv(String url)
+    {
+        String value = System.getenv(url);
+        if (value == null)
+        {
+            throw new IllegalArgumentException("Environment variable " + value + " is not set");
+        }
+        return value;
+    }
+
+}
